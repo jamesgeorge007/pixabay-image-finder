98e1e341a5c16b648bac3d3572a18486e00b2855
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-07-30 12:47:31 +0530

    fix: regression

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
 import axios from "axios";
 import ImageResults from "../image-results/ImageResults";
+
+const proxyurl = "https://cors-anywhere.herokuapp.com";
 
 class Search extends Component {
   constructor(props) {
@@ -11,24 +13,35 @@ class Search extends Component {
     this.state = {
       searchText: "",
       amount: 15,
-      apiUrl: "https://pixabay.com/api",
-      apiKey: "9655981-913147c3529ddaa8c3fe91d22",
+      apiUrl: `${proxyurl}/https://pixabay.com/api`,
+      apiKey: process.env.REACT_APP_API_KEY,
       images: [],
     };
   }
 
+  sendNetworkRequest = () => {
+    const { apiUrl, apiKey, searchText, amount } = this.state;
+    const url = `${apiUrl}/?key=${apiKey}&q=${searchText}&image_type=photo&per_page=${amount}&safesearch=true`;
+    return axios.get(url);
+  };
+
+  async componentDidUpdate(prevState) {
+    const { amount, searchText } = this.state;
+    if (prevState.searchText !== searchText || prevState.amount !== amount) {
+      try {
+        const { data } = await this.sendNetworkRequest();
+        this.setState({ images: data.hits });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
   onTextChange = (e) => {
     const val = e.target.value;
-    this.setState({ [e.target.name]: val }, () => {
+    this.setState({ [e.target.name]: val }, async () => {
       if (val === "") {
         this.setState({ images: [] });
-      } else {
-        axios
-          .get(
-            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
-          )
-          .then((res) => this.setState({ images: res.data.hits }))
-          .catch((err) => console.log(err));
       }
     });
   };
@@ -36,15 +49,14 @@ class Search extends Component {
   onAmountChange = (event, index, value) => this.setState({ amount: value });
 
   render() {
-    console.log(this.state.images);
     return (
       <div>
         <TextField
           name="searchText"
           value={this.state.searchText}
-          onChange={this.onTextChange}
           floatingLabelText="Search For Images"
           fullWidth={true}
+          onChange={this.onTextChange}
         />
         <br />
         <SelectField
@@ -62,7 +74,12 @@ class Search extends Component {
         <br />
         {this.state.images.length > 0 ? (
           <ImageResults images={this.state.images} />
-        ) : null}
+        ) : (
+          <Fragment>
+            <br />
+            <p>No results found</p>
+          </Fragment>
+        )}
       </div>
     );
   }
